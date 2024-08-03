e3e11b7e3c7a2dec4230a948ad5ef85593e3bb70
kunal <kunalhazra16@gmail.com>
2024-08-03 14:42:23 +0530

    Initial commit

--- ChildAircraft.java
+++ ChildAircraft.java
@@ -1,3 +1,18 @@
-public class ChildAircraft extends ParentAirCraft{
-    String name ="rafael";
+public class ChildAircraft extends ParentAirCraft {
+    String name = "rafael";
+
+//method overloading
+    public void getData(int a) {
+
+        System.out.println(a);
+
+
+    }
+
+    public void getData(String a) {
+        System.out.println(a);
+
+
+    }
+
 }
--- MAIN.java
+++ MAIN.java
@@ -3,8 +3,13 @@ public class MAIN {
         ParentAirCraft result = new ChildAircraft();
         ChildAircraft result1 = new ChildAircraft();
 //        ParentAirCraft result1 = new ParentAirCraft();
+        ChildAircraft n1 = new ChildAircraft();
+        n1.getData(8885858);
+        n1.getData("you will have a aircraft of your demand ");
 
         System.out.println(result1.name);
-
     }
+
 }
+
+
